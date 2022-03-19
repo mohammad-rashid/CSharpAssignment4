0326a831ae1a03bc39c6b56bd05c94d784a6ad71
mohammad-rashid <84977272+mohammad-rashid@users.noreply.github.com>
2022-03-19 19:29:26 +0530

    Added C Sharp Assignment 4

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loadLinesTask = Task.Run(async () =>
+            {
+
+                using (var stream = new StreamReader(
+                    File.OpenRead("A.txt")))
+                {
+                    var lines = new List<string>();
+                    string line;
+                   
+                    while ((line = await stream.ReadLineAsync()) != null)
+                    {
+                        lines.Add(line);
+                  
+                    }
+                    return lines;
+                }
+
+            });
+            loadLinesTask.ContinueWith(async t =>
+            {
+                using (var outputFile = new StreamWriter(File.OpenWrite("B.txt")))
+                {
+                    foreach (var line in loadLinesTask.Result)
+                    {
+                        await outputFile.WriteLineAsync(line);
+                    }
+                    
+                }
+            });
+        }
+        
+    }
+}
